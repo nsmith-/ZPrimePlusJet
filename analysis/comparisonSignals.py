a4231c209a1f910581c5ab14b88deb64e3f23ccd
Caterina <caterina.vernieri@cern.ch>
2016-11-21 14:22:58 -0600

    added more signal samples

--- analysis/comparisonSignals.py
+++ analysis/comparisonSignals.py
@@ -21,30 +21,38 @@ def main(options,args):
     lumi = options.lumi
     
     legname = {'ggHbb': 'ggH(b#bar{b}) MC@NLO',
-	   	'ggHbbp': 'ggH(b#bar{b}) powheg',
+	       'ggHbbp': 'ggH(b#bar{b}) powheg',
                'VBFHbb':'VBF H(b#bar{b})',
 	       'ZHbb': ' ZH(b#bar{b})',
-		'Phibb': ' Phi(125)(b#bar{b})'}
+	       'WHbb': 'WH(b#bar{b})',
+	       'tthbb': 'ttH(b#bar{b})',	   	
+	       'Phibb': ' Phi(125)(b#bar{b})'}
 
         
     tfiles = {'ggHbb': [idir+'/GluGluHToBB_M125_13TeV_amcatnloFXFX_pythia8_1000pb_weighted.root'],
-		'ggHbbp': [idir+'/GluGluHToBB_M125_13TeV_powheg_pythia8_1000pb_weighted.root'],
-               'VBFHbb': [idir+'/VBFHToBB_M125_13TeV_amcatnlo_pythia8_1000pb_weighted.root'],
-		'ZHbb': [idir+'/ZH_HToBB_ZToQQ_M125_13TeV_powheg_pythia8_1000pb_weighted.root'],
-		'Phibb':[idir+'/DMSpin0_ggPhibb1j_125_1000pb_weighted.root']
+       	      'ggHbbp': [idir+'/GluGluHToBB_M125_13TeV_powheg_pythia8_1000pb_weighted.root'],
+              'VBFHbb': [idir+'/VBFHToBB_M125_13TeV_amcatnlo_pythia8_1000pb_weighted.root'],
+	      'ZHbb': [idir+'/ZH_HToBB_ZToQQ_M125_13TeV_powheg_pythia8_1000pb_weighted.root'],
+	      'WHbb' : [idir+'/WminusH_HToBB_WToQQ_M125_13TeV_powheg_pythia8_1000pb_weighted.root', idir+'/WplusH_HToBB_WToQQ_M125_13TeV_powheg_pythia8_1000pb_weighted.root'],
+	      'tthbb' : [idir+'ttHTobb_M125_13TeV_powheg_pythia8_1000pb_weighted.root'],
+	      'Phibb':[idir+'/DMSpin0_ggPhibb1j_125_1000pb_weighted.root']
                }
 
     color = {'ggHbb': ROOT.kRed,
-	      'ggHbbp': ROOT.kBlue+2,
-               'VBFHbb': ROOT.kAzure+3,
-		'ZHbb': ROOT.kAzure+1,
-		'Phibb':ROOT.kRed-2
+	     'ggHbbp': ROOT.kBlue+2,
+             'VBFHbb': ROOT.kAzure+3,
+	     'ZHbb': ROOT.kAzure+1,
+	     'WHbb': ROOT.kPink+2,	
+	     'tthbb': ROOT.kOrange+1,
+	     'Phibb':ROOT.kRed-2
                }
 
     style = {'ggHbb': 2,
 	     'ggHbbp': 1,
              'VBFHbb': 2,
 	     'ZHbb': 1,
+	     'WHbb':1,
+	     'tthbb':1,		
 	     'Phibb':2
                }
         
@@ -55,6 +63,8 @@ def main(options,args):
     sigSamples['ggHbbp']  = sampleContainer(tfiles['ggHbbp']  , 1, lumi)
     sigSamples['VBFHbb'] = sampleContainer(tfiles['VBFHbb'], 1, lumi) 
     sigSamples['ZHbb'] = sampleContainer(tfiles['ZHbb'], 1, lumi ) 	
+    sigSamples['WHbb'] = sampleContainer(tfiles['WHbb'], 1, lumi )
+    sigSamples['tthbb'] = sampleContainer(tfiles['tthbb'], 1, lumi )	
     sigSamples['Phibb'] = sampleContainer(tfiles['Phibb'], 1, lumi*0.00126535)   
 
 
