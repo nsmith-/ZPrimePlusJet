b81a47e52c2e9a0f77fdae0a7a960fd1c2e2cab7
Cristina <criss.ms7@gmail.com>
2016-11-23 16:34:39 -0600

    update on control plots

--- analysis/controlPlotsZqqJet.py
+++ analysis/controlPlotsZqqJet.py
@@ -1,4 +1,4 @@
-import ROOT
+B1;3201;0cimport ROOT
 from ROOT import TFile, TTree, TChain, gPad, gDirectory
 from multiprocessing import Process
 from optparse import OptionParser
@@ -14,12 +14,10 @@ from sampleContainer import *
 
 ##############################################################################
 def main(options,args):
-    #idir = "/eos/uscms/store/user/lpchbb/ggHsample_V11/sklim-v0-28Oct/"
-    #odir = "plots_2016_10_31/"
     idir = options.idir
     odir = options.odir
     lumi = options.lumi
-
+    isData = options.isData
     
     legname = {'Zp100': "Z\'(100)",
                'Zp125':'Z\'(125)',
@@ -31,35 +29,44 @@ def main(options,args):
                'data': 'data'
                }
 
-    tfiles = {'Zp100': [idir+'/VectorDiJet1Jet_M100.root'],
-               'Zp125': [idir+'/VectorDiJet1Jet_M125.root'],
-               'Zp200': [idir+'/VectorDiJet1Jet_M200.root'],
-               'DY':  [idir+'/DY.root'],
-               'W':  [idir+'/W.root'],
-               'TTbar':  [idir+'/TTbar_madgraphMLM.root'],
-               'QCD': [idir+'/QCD.root'],
-               'data': [idir+'/JetHTRun2016.root']
-               }
+    tfiles = {'Zp100': [idir+'/VectorDiJet1Jet_M100_1000pb_weighted.root'],
+              'Zp125': [idir+'/VectorDiJet1Jet_M125_1000pb_weighted.root'],
+              'Zp200': [idir+'/VectorDiJet1Jet_M200_1000pb_weighted.root'],
+              'Diboson': [idir+'/WWTo4Q_13TeV_amcatnlo_1000pb_weighted.root',idir+'/ZZTo4Q_13TeV_amcatnlo_1000pb_weighted.root'],
+              'SingleTop':  [idir+'/ST_t-channel_antitop_4f_inclusiveDecays_13TeV_powheg_1000pb_weighted.root',
+                             idir+'/ST_t-channel_top_4f_inclusiveDecays_13TeV_powheg_1000pb_weighted.root',
+                             idir+'/ST_tW_antitop_5f_inclusiveDecays_13TeV_1000pb_weighted.root',
+                             idir+'/ST_tW_top_5f_inclusiveDecays_13TeV_1000pb_weighted.root'],
+              'TTbar':  [idir+'/TTJets_13TeV_1000pb_weighted.root'],
+              'DY':  [idir+'/DY_1000pb_weighted.root'],
+              'W':  [idir+'/WJets_1000pb_weighted.root'],
+              'QCD': [idir+'/QCD.root'],
+              'data': [idir+'/JetHTRun2016.root']
+              }
 
     color = {'Zp100': ROOT.kRed,
-               'Zp125': ROOT.kBlue-9,
-               'Zp200': ROOT.kMagenta-9,
-               'DY':  ROOT.kCyan,
-               'W':  ROOT.kTeal-1,
-               'TTbar':  ROOT.kGray,
-               'QCD': ROOT.kBlue+1,
-               'data': ROOT.kBlack
-               }
+             'Zp125': ROOT.kBlue-9,
+             'Zp200': ROOT.kMagenta-9,
+             'Diboson': ROOT.kOrange,
+             'SingleTop': ROOT.kRed-2,
+             'TTbar':  ROOT.kGray,
+             'DY':  ROOT.kCyan,
+             'W':  ROOT.kTeal-1,
+             'QCD': ROOT.kBlue+1,
+             'data': ROOT.kBlack
+             }
 
     style = {'Zp100': 2,
-               'Zp125': 3,
-               'Zp200':4,
-               'DY': 1,
-               'W': 1,
-               'TTbar': 1,
-               'QCD': 1,
-               'data': 1
-               }
+             'Zp125': 3,
+             'Zp200':4,
+             'Diboson': 1,
+             'SingleTop': 1,
+             'TTbar': 1,
+             'DY': 1,
+             'W': 1
+             'QCD': 1,
+             'data': 1
+             }
 
         
     print "Signals... "
@@ -69,29 +76,36 @@ def main(options,args):
     sigSamples['Zp200'] = sampleContainer(tfiles['Zp200'], 1, lumi * 1) # 1fb
     print "Backgrounds..."
     bkgSamples = {}
+    #bkgSamples['SingleTop'] = sampleContainer(tfiles['SingleTop'], 1, lumi)
+    #bkgSamples['Diboson'] = sampleContainer(tfiles['Diboson'], 1, lumi)
+    bkgSamples['TTbar']  = sampleContainer(tfiles['TTbar'], 1, lumi)
     bkgSamples['DY'] = sampleContainer(tfiles['DY'], 1, lumi)
     bkgSamples['W']  = sampleContainer(tfiles['W'], 1, lumi)
-    bkgSamples['TTbar']  = sampleContainer(tfiles['TTbar'], 1, lumi)
-    # this 100 scale factor...just makes the QCD run faster, to use all the QCD, make the SF = 1
     bkgSamples['QCD'] = sampleContainer(tfiles['QCD'], 100, lumi) 
 
-    isData = True;
-    dataSample = sampleContainer(tfiles['data'],10,lumi,isData)
+    if isData:
+        dataSample = sampleContainer(tfiles['data'],10,lumi,isData)
 
-    ofile = ROOT.TFile.Open(odir+'/PlotsGGH.root','recreate')
+    ofile = ROOT.TFile.Open(odir+'/PlotsZqq.root','recreate')
 
-    # plots = ['h_pt_ak8','h_msd_ak8','h_dbtag_ak8','h_n_ak4','h_n_ak4_dR0p8','h_pt_ak8_dbtagCut','h_msd_ak8_dbtagCut','h_t21_ak8','h_t32_ak8']
     plots = ['h_pt_ak8','h_msd_ak8','h_n2b1sd_ak8','h_n2b1sdddt_ak8','h_t21_ak8','h_t21ddt_ak8']
     canvases = []
     for plot in plots:
+        hall = {}
         hs = {}
         for process, s in sigSamples.iteritems():
             hs[process] = getattr(s,plot)
+            hall[process] = getattr(s,plot)
         hb = {}
         for process, s in bkgSamples.iteritems():
             hb[process] = getattr(s,plot)
-        hd = getattr(dataSample,plot)
-        c = makeCanvasComparisonStackWData(hd,hs,hb,legname,color,style,plot.replace('h_','stack_'),odir,lumi,ofile)
+            hall[process] = getattr(s,plot)
+        if isData:
+            hd = getattr(dataSample,plot)
+            c = makeCanvasComparisonStackWData(hd,hs,hb,legname,color,style,plot.replace('h_','stack_'),odir,lumi,ofile)
+        else:
+            c = makeCanvasComparisonStack(hs,hb,legname,color,style,'ggHbb',plot.replace('h_','stack_'),odir,lumi,ofile)
+            c1 = makeCanvasComparison(hall,legname,color,style,plot.replace('h_','signalcomparison_'),odir,lumi,ofile,True)
         canvases.append(c)
 
 
