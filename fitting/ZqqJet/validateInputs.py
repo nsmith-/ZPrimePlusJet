6aae3558c6dccade907eb7fe2df2e3470f1d87ad
nhanvtran <nhan.v.tran@gmail.com>
2016-11-27 16:58:17 -0600

    udpating the validation code

--- fitting/ZqqJet/validateInputs.py
+++ fitting/ZqqJet/validateInputs.py
@@ -23,52 +23,92 @@ def main(options,args):
 
 	# Load the input histograms
 	f = r.TFile("base.root");
+	fr  = r.TFile("ralphabase.root");
+
 	wp = f.Get("w_pass_cat1");
 	wf = f.Get("w_fail_cat1");
-
-	fr  = r.TFile("ralphabase.root");
 	wpr = fr.Get("w_pass_cat1");
 	wfr = fr.Get("w_fail_cat1");
 
 	# wp.Print();
 	# wf.Print();
-	# wpr.Print();
+	wpr.Print();
 	wfr.Print();
 
+	for i in range(5): drawCategory(f,fr,"cat"+str(i+1));
+
+###############################################################
+
+def drawCategory(f,fr,catname):
+
+	wp = f.Get("w_pass_"+catname);
+	wf = f.Get("w_fail_"+catname);
+	wpr = fr.Get("w_pass_"+catname);
+	wfr = fr.Get("w_fail_"+catname);
+
 	rrv   = wp.var("x"); 
-	dh_w_p  = wp.data("wqq_pass_cat1");
-	dh_z_p  = wp.data("zqq_pass_cat1");
-	dh_t_p  = wp.data("tqq_pass_cat1");
-	ph_q_p  = wpr.pdf("qcd_pass_cat1");
-	ph_q_f  = wfr.pdf("qcd_fail_cat1");
+	dh_w_p  = wp.data("wqq_pass_"+catname);
+	dh_z_p  = wp.data("zqq_pass_"+catname);
+	dh_t_p  = wp.data("tqq_pass_"+catname);
+	ph_q_p  = wpr.pdf("qcd_pass_"+catname);
+	dh_d_p  = wp.data("data_obs_pass_"+catname);
+	dh_w_f  = wf.data("wqq_fail_"+catname);
+	dh_z_f  = wf.data("zqq_fail_"+catname);
+	dh_t_f  = wf.data("tqq_fail_"+catname);
+	ph_q_f  = wfr.pdf("qcd_fail_"+catname);
+	dh_d_f  = wf.data("data_obs_fail_"+catname);
 
- 	frame = rrv.frame();
-  	dh_w_p.plotOn(frame, r.RooFit.DrawOption("pe"), r.RooFit.MarkerColor(r.kRed));
-  	dh_z_p.plotOn(frame);
-	dh_t_p.plotOn(frame, r.RooFit.DrawOption("pe"), r.RooFit.MarkerColor(r.kBlue));
-	ph_q_p.plotOn(frame, r.RooFit.LineColor(r.kBlue), r.RooFit.LineWidth(10 ));
-	ph_q_f.plotOn(frame, r.RooFit.LineColor(r.kRed));
+ 	frame_p = rrv.frame();
+  	dh_w_p.plotOn(frame_p, r.RooFit.DrawOption("pe"), r.RooFit.MarkerColor(r.kRed));
+  	dh_z_p.plotOn(frame_p, r.RooFit.DrawOption("pe"), r.RooFit.MarkerColor(r.kGreen));
+	dh_t_p.plotOn(frame_p, r.RooFit.DrawOption("pe"), r.RooFit.MarkerColor(r.kBlue));
+	ph_q_p.plotOn(frame_p, r.RooFit.LineColor(r.kBlue), r.RooFit.LineWidth(10 ));
+	dh_d_p.plotOn(frame_p, r.RooFit.DrawOption("pe"), r.RooFit.MarkerColor(r.kBlack));
+	ph_q_f.plotOn(frame_p, r.RooFit.LineColor(r.kRed));
 
-  	c = r.TCanvas("c","c",1000,800);
-  	frame.Draw();
-  	c.SaveAs("plots/test.pdf");
+ 	frame_f = rrv.frame();
+  	dh_w_f.plotOn(frame_f, r.RooFit.DrawOption("pe"), r.RooFit.MarkerColor(r.kRed));
+  	dh_z_f.plotOn(frame_f, r.RooFit.DrawOption("pe"), r.RooFit.MarkerColor(r.kGreen));
+	dh_t_f.plotOn(frame_f, r.RooFit.DrawOption("pe"), r.RooFit.MarkerColor(r.kBlue));
+	ph_q_f.plotOn(frame_f, r.RooFit.LineColor(r.kRed));
+	dh_d_f.plotOn(frame_f, r.RooFit.DrawOption("pe"), r.RooFit.MarkerColor(r.kBlack));
+
+  	cp = r.TCanvas("cp","cp",1000,800);
+  	frame_p.Draw();
+  	cp.SaveAs("plots/mass-pass-"+catname+".pdf");
+  	cp.SaveAs("plots/mass-pass-"+catname+".png");
   	r.gPad.SetLogy();
-  	c.SaveAs("plots/test-log.pdf");
+  	cp.SaveAs("plots/mass-pass-"+catname+"-log.pdf");
+  	cp.SaveAs("plots/mass-pass-"+catname+"-log.png");
+
+  	cf = r.TCanvas("cf","cf",1000,800);
+  	frame_f.Draw();
+  	cf.SaveAs("plots/mass-fail-"+catname+".pdf");
+  	cf.SaveAs("plots/mass-fail-"+catname+".png");
+  	r.gPad.SetLogy();
+  	cf.SaveAs("plots/mass-fail-"+catname+"-log.pdf");
+  	cf.SaveAs("plots/mass-fail-"+catname+"-log.png");
+
 
   	######## Some print outs
-  	print "-------"
-  	print "qcd_fail_cat1_Bin1 = ", wfr.var("qcd_fail_cat1_Bin1").getValV();
-  	print "qcdeff = ", wpr.var("qcdeff").getValV();
+ #  	print "-------"
+ #  	print "qcd_fail_cat1_Bin1 = ", wfr.var("qcd_fail_cat1_Bin1").getValV();
+ #  	print "qcdeff = ", wpr.var("qcdeff").getValV();
 
-	# "Var_RhoPol_Bin_530.0_-10.138"
-	# 	"Var_Pol_Bin_530.0_-10.138_0"
-	# 		"r0","p1"
-	# 	"Var_Pol_Bin_530.0_-10.138_1"
-	# 		"r1","pr11"
-  	print "r0 = ", wpr.var("r0").getValV();
-  	print "p1 = ", wpr.var("p1").getValV();
-  	print "r1 = ", wpr.var("r1").getValV();
-  	print "pr11 = ", wpr.var("pr11").getValV();
+	# # "Var_RhoPol_Bin_530.0_-10.138"
+	# # 	"Var_Pol_Bin_530.0_-10.138_0"
+	# # 		"r0","p1"
+	# # 	"Var_Pol_Bin_530.0_-10.138_1"
+	# # 		"r1","pr11"
+ #  	print "r0 = ", wpr.var("r0").getValV();
+ #  	print "p1 = ", wpr.var("p1").getValV();
+ #  	print "r1 = ", wpr.var("r1").getValV();
+ #  	print "pr11 = ", wpr.var("pr11").getValV();
+
+###############################################################
+###############################################################
+###############################################################
+
 
 def do2DHistInputs(fn):
 
@@ -82,6 +122,10 @@ def do2DHistInputs(fn):
 	h2s.append( tf.Get("zqq_fail") );
 	h2s.append( tf.Get("tqq_pass") );
 	h2s.append( tf.Get("tqq_fail") );
+	h2s.append( tf.Get("zqq100_pass") );
+	h2s.append( tf.Get("zqq100_fail") );
+	h2s.append( tf.Get("zqq200_pass") );
+	h2s.append( tf.Get("zqq200_fail") );
 
 	for h2 in h2s:
 		for ipt in range(h2.GetNbinsY()):
