c043716e24bfe17d974311efa977c4b7c2892c1b
Javier Duarte <jduarte1@fnal.gov>
2017-01-19 15:38:32 -0600

    update hadd to print bad files

--- sklimming/hadd.py
+++ sklimming/hadd.py
@@ -33,30 +33,43 @@ def main(options,args):
     OutDir = options.odir
 
     tags = []
-    tags.append([ 'JetHTRun2016B_23Sep2016_v3/', 0] )
-    tags.append([ 'JetHTRun2016B_23Sep2016_v1/', 0] )
-    tags.append([ 'JetHTRun2016C_23Sep2016_v1/', 0] )
+    #tags.append([ 'JetHTRun2016B_23Sep2016_v3/', 0] )
+    #tags.append([ 'JetHTRun2016B_23Sep2016_v1/', 0] )
+    #tags.append([ 'JetHTRun2016C_23Sep2016_v1/', 0] )
     tags.append([ 'JetHTRun2016D_23Sep2016_v1/', 0] )
-    tags.append([ 'JetHTRun2016E_23Sep2016_v1/', 0] )
+    #tags.append([ 'JetHTRun2016E_23Sep2016_v1/', 0] )
     tags.append([ 'JetHTRun2016F_23Sep2016_v1/', 0] )
-    tags.append([ 'JetHTRun2016G_23Sep2016_v1/', 0] )
-    tags.append([ 'JetHTRun2016H_PromptReco_v3/', 0] )
-    tags.append([ 'SingleMuonRun2016B_PromptReco_v2/', 0] )
-    tags.append([ 'SingleMuonRun2016C_PromptReco_v2/', 0] )
-    tags.append([ 'SingleMuonRun2016D_PromptReco_v2/', 0] )
-    tags.append([ 'SingleMuonRun2016E_PromptReco_v2/', 0] )
-    tags.append([ 'SingleMuonRun2016F_PromptReco_v1/', 0] )
-    tags.append([ 'SingleMuonRun2016G_PromptReco_v1/', 0] )
-    tags.append([ 'SingleMuonRun2016H_PromptReco_v2/', 0] )
+    #tags.append([ 'JetHTRun2016G_23Sep2016_v1/', 0] )
+    tags.append([ 'JetHTRun2016H_PromptReco_v1/', 0] )
+    tags.append([ 'JetHTRun2016H_PromptReco_v2/', 0] )
+    #tags.append([ 'JetHTRun2016H_PromptReco_v3/', 0] )
+    tags.append([ 'SingleMuonRun2016B_23Sep2016_v1/', 0] )
+    tags.append([ 'SingleMuonRun2016B_23Sep2016_v3/', 0] )
+    tags.append([ 'SingleMuonRun2016C_23Sep2016_v1/', 0] )
+    tags.append([ 'SingleMuonRun2016D_23Sep2016_v1/', 0] )
+    tags.append([ 'SingleMuonRun2016E_23Sep2016_v1/', 0] )
+    tags.append([ 'SingleMuonRun2016F_23Sep2016_v1/', 0] )
+    tags.append([ 'SingleMuonRun2016G_23Sep2016_v1/', 0] )
+    tags.append([ 'SingleMuonRun2016H_PromptReco_v1/', 0] )
+    #tags.append([ 'SingleMuonRun2016H_PromptReco_v2/', 0] )
+    #tags.append([ 'SingleMuonRun2016H_PromptReco_v3/', 0] )
 
     postfix = ''
     for i in range(len(tags)):
         basename = tags[i][0].replace('/','') + '.root'
-        filesToConvert = getFilesRecursively(DataDir,tags[i][0],None,'sklim')
+        filesToConvert, badFiles = getFilesRecursively(DataDir,tags[i][0],None,'sklim')
         print "files To Convert = ",filesToConvert
-        print 'hadd -f %s/%s %s'%(OutDir,basename,' '.join(filesToConvert))
-        os.system('echo hadd -f %s/%s %s >> hadd_command_%s.sh'%(OutDir,basename,' '.join(filesToConvert),basename))
-        os.system('hadd -f %s/%s %s'%(OutDir,basename,' '.join(filesToConvert)))
+        print "bad files = ", badFiles
+        haddCommand = 'hadd -f %s/%s %s'%(OutDir,basename,' '.join(filesToConvert))
+        print haddCommand
+        with open('hadd_command_%s.sh'%basename,'w') as f:
+            f.write(haddCommand)
+        with open('bad_files_%s.txt'%basename,'w') as f:
+            for badFile in badFiles:
+                f.write(badFile+'\n')
+
+        os.system('source hadd_command_%s.sh'%basename)
+
 
 
 
@@ -70,6 +83,7 @@ def getFilesRecursively(dir,searchstring,additionalstring = None, skipString = N
         theadditionalstring = additionalstring
 
     cfiles = []
+    badfiles = []
     for root, dirs, files in os.walk(dir+'/'+thesearchstring):
         nfiles = len(files)
         for ifile, file in enumerate(files):
@@ -81,23 +95,27 @@ def getFilesRecursively(dir,searchstring,additionalstring = None, skipString = N
                 if f.IsZombie():
                     print 'file is zombie'
                     f.Close()
+                    badfiles.append(os.path.join(root, file))                    
                     continue
                 elif not f.Get('Events'):
                     print 'tree is false'
                     f.Close()
+                    badfiles.append(os.path.join(root, file))                    
                     continue
                 elif not f.Get('Events').InheritsFrom('TTree'):
                     print 'tree is not a tree'
                     f.Close()
+                    badfiles.append(os.path.join(root, file))                    
                     continue
                 else:
                     f.Close()
                     cfiles.append(os.path.join(root, file))                    
-            except KeyError:
+            except:
                 print 'could not open file or tree'
+                badfiles.append(os.path.join(root, file))                    
                 continue
                 
-    return cfiles
+    return cfiles, badfiles
 
 
 
@@ -108,7 +126,7 @@ if __name__ == '__main__':
     parser.add_option('--train', action='store_true', dest='train', default=False, help='train')
     parser.add_option("--lumi", dest="lumi", default = 30,type=float,help="luminosity", metavar="lumi")
     parser.add_option('-i','--idir', dest='idir', default = 'data/',help='directory with bacon bits', metavar='idir')
-    parser.add_option('-o','--odir', dest='odir', default = 'skim/',help='directory to write skimmed backon bits', metavar='odir')
+    parser.add_option('-o','--odir', dest='odir', default = 'skim/',help='directory to write hadded bits', metavar='odir')
 
     (options, args) = parser.parse_args()
 
