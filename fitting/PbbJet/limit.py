655a2b5bb3e7ff03af07d7b8709e9c07e040cac1
Javier Duarte <jduarte@caltech.edu>
2017-01-19 22:28:16 -0600

    update limit code (with f-test, gof test)

--- fitting/PbbJet/limit.py
+++ fitting/PbbJet/limit.py
@@ -32,39 +32,129 @@ def plotgaus(iFName,injet,iLabel):
     lCan.SaveAs(iLabel+".pdf")
     #end()
 
-def plotftest(iToys,iCentral,prob,iLabel):
-    lCan   = r.TCanvas(str(iLabel),str(iLabel),800,600)
-    lH = r.TH1F(iLabel+"hist",iLabel+"hist",20,min(min(iToys),iCentral)-20,max(max(iToys),iCentral)+20)
-    lH.GetXaxis().SetTitle("#DeltaLL")  
+def plotftest(iToys,iCentral,prob,iLabel,options):
+    lCan   = r.TCanvas(str(iLabel),str(iLabel),800,600)    
+    lCan.SetLeftMargin(0.12) 
+    lCan.SetBottomMargin(0.12)
+    lCan.SetRightMargin(0.1)
+    lCan.SetTopMargin(0.1)
+    
+    if options.method=='FTest':
+        lH = r.TH1F(iLabel+"hist",iLabel+"hist",70,0,max(max(iToys),iCentral)+1)
+        lH_cut = r.TH1F(iLabel+"hist",iLabel+"hist",70,0,max(max(iToys),iCentral)+1)
+    else:
+        lH = r.TH1F(iLabel+"hist",iLabel+"hist",70,0,max(max(iToys),iCentral)+100)
+        lH_cut = r.TH1F(iLabel+"hist",iLabel+"hist",70,0,max(max(iToys),iCentral)+100)
+    
+    if options.method=='FTest':
+        lH.GetXaxis().SetTitle("F = #frac{-2log(#lambda_{1}/#lambda_{2})/(p_{2}-p_{1})}{-2log#lambda_{2}/(n-p_{2})}")
+        lH.GetXaxis().SetTitleSize(0.025)
+        lH.GetXaxis().SetTitleOffset(2)
+        lH.GetYaxis().SetTitle("Pseudodatasets")
+        lH.GetYaxis().SetTitleOffset(0.85)
+    else:        
+        lH.GetXaxis().SetTitle("-2log#lambda")  
+        lH.GetYaxis().SetTitle("Pseudodatasets")
+        lH.GetYaxis().SetTitleOffset(0.85)
     for val in iToys:
         lH.Fill(val)
-    lH.Draw("hist")
-    lLine  = r.TLine(iCentral,0,iCentral,lH.GetMaximum())
-    lLine.SetLineColor(2)
+        if val > iCentral:
+            lH_cut.Fill(val)
+    lH.SetMarkerStyle(20)
+    lH.Draw("pez")
+    lLine  = r.TArrow(iCentral,0.25*lH.GetMaximum(),iCentral,0)
+    lLine.SetLineColor(r.kBlue+1)
+    lLine.SetLineWidth(2)
+
+    lH_cut.SetLineColor(r.kViolet-10)
+    lH_cut.SetFillColor(r.kViolet-10)
+    lH_cut.Draw("histsame")
+    
+    if options.method=='FTest':
+        fdist = r.TF1("fDist", "[0]*TMath::FDist(x, [1], [2])", 0,max(max(iToys),iCentral)+1)
+        fdist.SetParameter(0,lH.Integral()*((max(max(iToys),iCentral)+1)/70.))
+        fdist.SetParameter(1,options.p2-options.p1)
+        fdist.SetParameter(2,options.n-options.p2)
+        fdist.Draw('same')
+        #lH.Fit(fdist,'mle')
+    else:        
+        chi2_func = r.TF1('chisqpdf','[0]*ROOT::Math::chisquared_pdf(x,[1])',0,max(max(iToys),iCentral)+100)
+        chi2_func.SetParameter(0,lH.Integral())
+        chi2_func.SetParameter(1,50)
+        chi2_func.Draw('same')
+        lH.Fit(chi2_func,"mle")
+    lH.Draw("pezsame")
     lLine.Draw()
-    lText  = r.TPaveText(0.7,0.7,0.88,0.88,"NDCNB") 
-    lText.SetFillColor(0)
-    lText.SetBorderSize(0)
-    lText.AddText("F-test prob = "+str(prob)) if iLabel.find('f') > -1 else  lText.AddText("Goodness prob = "+str(prob))
-    lText.Draw()
-    lCan.SaveAs(iLabel+".png")
-    lCan.SaveAs(iLabel+".pdf")
+        
+    tLeg = r.TLegend(0.6,0.6,0.89,0.89)
+    tLeg.SetLineColor(r.kWhite)
+    tLeg.SetLineWidth(0)
+    tLeg.SetFillStyle(0)
+    tLeg.SetTextFont(42)
+    tLeg.AddEntry(lH,"toy data","lep")
+    tLeg.AddEntry(lLine,"observed = %.1f"%iCentral,"l")
+    tLeg.AddEntry(lH_cut,"p-value = %.2f"%(1-prob),"f")
+    if options.method=='FTest':
+        #tLeg.AddEntry(fdist,"f-dist fit, ndf = (%.1f #pm %.1f, %.1f #pm %.1f) "%(fdist.GetParameter(1),fdist.GetParError(1),fdist.GetParameter(2),fdist.GetParError(2)),"l")
+        tLeg.AddEntry(fdist,"F-dist, ndf = (%.0f, %.0f) "%(fdist.GetParameter(1),fdist.GetParameter(2)),"l")
+    else:
+        tLeg.AddEntry(chi2_func,"#chi^{2} fit, ndf = %.1f #pm %.1f"%(chi2_func.GetParameter(1),chi2_func.GetParError(1)),"l")
+            
+    tLeg.Draw("same")
+
+    l = r.TLatex()
+    l.SetTextAlign(11)
+    l.SetTextSize(0.06)
+    l.SetTextFont(62)
+    l.SetNDC()
+    l.DrawLatex(0.12,0.91,"CMS")
+    l.SetTextSize(0.05)
+    l.SetTextFont(52)
+    if options.isData:
+        l.DrawLatex(0.23,0.91,"Preliminary")
+    else:
+        l.DrawLatex(0.23,0.91,"Simulation")
+    l.SetTextFont(42)
+    l.DrawLatex(0.76,0.91,"%.1f fb^{-1}"%options.lumi)
+    l.SetTextFont(52)
+    l.SetTextSize(0.045)
+    
+
+    
+    lCan.SaveAs(options.odir+'/'+iLabel+".pdf")
+    lCan.SaveAs(options.odir+'/'+iLabel+".C")
     #end()
 
 def nllDiff(iFName1,iFName2):
-    lFile1 = r.TFile(iFName1)
+    lFile1 = r.TFile.Open(iFName1)
     lTree1 = lFile1.Get("limit")
-    lFile2 = r.TFile(iFName2)
+    lFile2 = r.TFile.Open(iFName2)
     lTree2 = lFile2.Get("limit")
     lDiffs=[]
     for i0 in range(0,lTree1.GetEntries()):
         lTree1.GetEntry(i0)
         lTree2.GetEntry(i0)
-        lDiffs.append(lTree1.nll0-lTree2.nll0)
+        diff = 2*(lTree1.nll-lTree1.nll0)-2*(lTree2.nll-lTree2.nll0)
+        lDiffs.append(diff)
+    return lDiffs
+
+
+def fStat(iFName1,iFName2,p1,p2,n):
+    lFile1 = r.TFile.Open(iFName1)
+    lTree1 = lFile1.Get("limit")
+    lFile2 = r.TFile.Open(iFName2)
+    lTree2 = lFile2.Get("limit")
+    lDiffs=[]
+    for i0 in range(0,lTree1.GetEntries()):
+        lTree1.GetEntry(i0)
+        lTree2.GetEntry(i0)
+        if lTree1.limit-lTree2.limit>0:
+            F = (lTree1.limit-lTree2.limit)/(p2-p1)/(lTree2.limit/(n-p2))
+            lDiffs.append(F)
     return lDiffs
 
 def goodnessVals(iFName1):
-    lFile1 = r.TFile(iFName1)
+    lFile1 = r.TFile.Open(iFName1)
     lTree1 = lFile1.Get("limit")
     lDiffs=[]
     for i0 in range(0,lTree1.GetEntries()):
@@ -72,41 +162,44 @@ def goodnessVals(iFName1):
         lDiffs.append(lTree1.limit)
     return lDiffs
 
-def ftest(base,alt,ntoys,iLabel):
-    os.system('combine -M MultiDimFit %s --rMax 100 --rMin -100 --saveNLL' % alt)
-    os.system('mv higgsCombineTest.MultiDimFit.mH120.root base1.root')
-    os.system('combine -M MultiDimFit %s  --rMax 100 --rMin -100 --saveNLL'% base)
-    os.system('mv higgsCombineTest.MultiDimFit.mH120.root base2.root')
-    os.system('combine -M GenerateOnly %s --rMax 100 --rMin -100 --toysFrequentist -t %i --expectSignal 10 --saveToys ' % (base,ntoys))
-    os.system('combine -M MultiDimFit  %s --rMax 100 --rMin -100 -t %i --saveNLL --toysFile higgsCombineTest.GenerateOnly.mH120.123456.root' % (alt,ntoys))
-    os.system('mv higgsCombineTest.MultiDimFit.mH120.123456.root toys1.root')
-    os.system('combine -M MultiDimFit  %s --rMax 100 --rMin -100 -t %i --saveNLL --toysFile higgsCombineTest.GenerateOnly.mH120.123456.root' % (base,ntoys))
-    os.system('mv higgsCombineTest.MultiDimFit.mH120.123456.root toys2.root')
-    #os.system('rm higgsCombineTest.GenerateOnly.mH120.123456.root')
-    nllBase=nllDiff("base1.root","base2.root")
-    nllToys=nllDiff("toys1.root","toys2.root")
+def ftest(base,alt,ntoys,iLabel,options):
+    if not options.justPlot:
+        os.system('combine -M GoodnessOfFit %s  --rMax 50 --rMin -50 --algorithm saturated --freezeNuisances tqqnormSF,tqqeffSF'% base)
+        os.system('mv higgsCombineTest.GoodnessOfFit.mH120.root %s/base1.root'%options.odir)
+        os.system('combine -M GoodnessOfFit %s --rMax 50 --rMin -50 --algorithm saturated --freezeNuisances tqqnormSF,tqqeffSF' % alt)
+        os.system('mv higgsCombineTest.GoodnessOfFit.mH120.root %s/base2.root'%options.odir)
+        os.system('combine -M GenerateOnly %s --rMax 50 --rMin -50 --toysFrequentist -t %i --expectSignal 0 --saveToys --freezeNuisances tqqnormSF,tqqeffSF' % (base,ntoys))
+        os.system('mv higgsCombineTest.GenerateOnly.mH120.123456.root %s/'%options.odir)
+        os.system('combine -M GoodnessOfFit %s --rMax 50 --rMin -50 -t %i --toysFile %s/higgsCombineTest.GenerateOnly.mH120.123456.root --algorithm saturated --freezeNuisances tqqnormSF,tqqeffSF' % (base,ntoys,options.odir))
+        os.system('mv higgsCombineTest.GoodnessOfFit.mH120.123456.root %s/toys1.root'%options.odir)
+        os.system('combine -M GoodnessOfFit %s --rMax 50 --rMin -50 -t %i --toysFile %s/higgsCombineTest.GenerateOnly.mH120.123456.root --algorithm saturated --freezeNuisances tqqnormSF,tqqeffSF' % (alt,ntoys,options.odir))
+        os.system('mv higgsCombineTest.GoodnessOfFit.mH120.123456.root %s/toys2.root'%options.odir)
+    nllBase=fStat("%s/base1.root"%options.odir,"%s/base2.root"%options.odir,options.p1,options.p2,options.n)
+    nllToys=fStat("%s/toys1.root"%options.odir,"%s/toys2.root"%options.odir,options.p1,options.p2,options.n)
     lPass=0
     for val in nllToys:
         print val,nllBase[0]
-        if val < nllBase[0]:
+        if nllBase[0] > val:
             lPass+=1
-    print "ftest prob",float(lPass)/float(len(nllToys))
-    plotftest(nllToys,nllBase[0],float(lPass)/float(len(nllToys)),iLabel)
+    print "FTest p-value",float(lPass)/float(len(nllToys))
+    plotftest(nllToys,nllBase[0],float(lPass)/float(len(nllToys)),iLabel,options)
     return float(lPass)/float(len(nllToys))
 
-def goodness(base,ntoys,iLabel):
-    os.system('combine -M GoodnessOfFit %s --rMax 100 --rMin -100  --algorithm saturated' % base)
-    os.system('mv higgsCombineTest.GoodnessOfFit.mH120.root goodbase.root')
-    os.system('combine -M GoodnessOfFit %s --rMax 100 --rMin -100 --toysFrequentist -t %i  --algorithm saturated ' % (base,ntoys))
-    os.system('mv higgsCombineTest.GoodnessOfFit.mH120.123456.root goodtoys.root')
-    nllBase=goodnessVals('goodbase.root')
-    nllToys=goodnessVals('goodtoys.root')
+def goodness(base,ntoys,iLabel,options):
+    if not options.justPlot:
+        os.system('combine -M GoodnessOfFit %s --rMax 50 --rMin -50  --algorithm saturated --freezeNuisances tqqnormSF,tqqeffSF' % base)
+        os.system('mv higgsCombineTest.GoodnessOfFit.mH120.root %s/goodbase.root'%options.odir)
+        os.system('combine -M GenerateOnly %s --rMax 50 --rMin -50 --toysFrequentist -t %i --expectSignal 0 --saveToys --freezeNuisances tqqnormSF,tqqeffSF' % (base,ntoys))
+        os.system('combine -M GoodnessOfFit %s --rMax 50 --rMin -50 --toysFile higgsCombineTest.GenerateOnly.mH120.123456.root -t %i --algorithm saturated --freezeNuisances tqqnormSF,tqqeffSF' % (base,ntoys))
+        os.system('mv higgsCombineTest.GoodnessOfFit.mH120.123456.root %s/goodtoys.root'%(options.odir))
+    nllBase=goodnessVals('%s/goodbase.root'%options.odir)
+    nllToys=goodnessVals('%s/goodtoys.root'%options.odir)
     lPass=0
     for val in nllToys:
-        if val > nllBase[0]:
+        if nllBase[0] > val:
             lPass+=1
-    print "Goodness prob",float(lPass)/float(len(nllToys))
-    plotftest(nllToys,nllBase[0],float(lPass)/float(len(nllToys)),iLabel)
+    print "GoodnessOfFit p-value",float(lPass)/float(len(nllToys))
+    plotftest(nllToys,nllBase[0],float(lPass)/float(len(nllToys)),iLabel,options)
     return float(lPass)/float(len(nllToys))
 
 def bias(base,alt,ntoys,mu,iLabel):
@@ -115,6 +208,11 @@ def bias(base,alt,ntoys,mu,iLabel):
     os.system('rm  higgsCombineTest.MaxLikelihoodFit.mH120.123456.root')
     os.system('mv  mlfit.root toys.root')
     plotgaus("toys.root",mu,"pull"+iLabel)
+
+def fit(base,options):
+    os.system('combine -M MaxLikelihoodFit %s -v 2 --freezeNuisances tqqeffSF,tqqnormSF --rMin=-50 --rMax=50 --saveNormalizations --plot --saveShapes --saveWithUncertainties --minimizerTolerance 0.001 --minimizerStrategy 2'%base)
+    os.system('mv mlfit.root %s/'%options.odir)
+    os.system('mv higgsCombineTest.MaxLikelihoodFit.mH120.root %s/'%options.odir)
     
 def limit(base):
     os.system('combine -M Asymptotic %s  ' % base)
@@ -159,14 +257,32 @@ def generate(mass,toys):
 
 if __name__ == "__main__":
     parser = OptionParser()
-    parser.add_option('--mass'   ,action='store',type='int',dest='mass'   ,default=125, help='mass')
-    parser.add_option('--toys'   ,action='store',type='int',dest='toys'   ,default=200,help='mass')
+    parser.add_option('-m','--mass'   ,action='store',type='int',dest='mass'   ,default=125, help='mass')
+    parser.add_option('-n','--n' ,action='store',type='int',dest='n'   ,default=5*20, help='number of bins')
+    parser.add_option('--p1' ,action='store',type='int',dest='p1'   ,default=9, help='number of parameters for default datacard (p1 > p2)')
+    parser.add_option('--p2' ,action='store',type='int',dest='p2'   ,default=12, help='number of parameters for alternative datacard (p2 > p1)')
+    parser.add_option('-t','--toys'   ,action='store',type='int',dest='toys'   ,default=200, help='number of toys')
     parser.add_option('--sig'    ,action='store',type='int',dest='sig'    ,default=1 ,help='sig')
+    parser.add_option('-d','--datacard'   ,action='store',type='string',dest='datacard'   ,default='card_rhalphabet.txt', help='datacard name')
+    parser.add_option('--datacard-alt'   ,action='store',type='string',dest='datacardAlt'   ,default='card_rhalphabet_alt.txt', help='alternative datacard name')
+    parser.add_option('-M','--method'   ,dest='method'   ,default='GoodnessOfFit', 
+                      choices=['GoodnessOfFit','FTest','Asymptotic','Bias'],help='combine method to use')
+    parser.add_option('-o','--odir', dest='odir', default = 'plots/',help='directory to write plots and output toys', metavar='odir')
+    parser.add_option('--just-plot', action='store_true', dest='justPlot', default=False, help='just plot')
+    parser.add_option('--data', action='store_true', dest='isData', default=False, help='is data')
+    parser.add_option('-l','--lumi'   ,action='store',type='float',dest='lumi'   ,default=36.4, help='lumi')
+
     (options,args) = parser.parse_args()
-    print options
 
     import tdrstyle
     tdrstyle.setTDRStyle()
+    
+    r.gStyle.SetOptStat(0)
+    r.gStyle.SetOptFit(0)
+    r.gStyle.SetOptTitle(0)
+    r.gStyle.SetPaintTextFormat("1.2g")
+    r.gROOT.SetBatch()
+    r.RooMsgService.instance().setGlobalKillBelow(r.RooFit.FATAL)
 
     #setupMC('ZQQ_'+str(options.mass),options.mass,"mc")
     #setup('ZQQ_'+str(options.mass),options.mass,"ralpha","base")
@@ -174,8 +290,31 @@ if __name__ == "__main__":
     #generate(options.mass,options.toys)
 
     #limit('combined.txt')
-    #ftest('combined.txt','combined_r2p1.txt',100,'ftestpol2')	
-
     #goodness('card_ralpha.txt',options.toys,"goodness"+str(options.mass))
-    bias('combined.txt','combined.txt',options.toys,options.sig,"fitbase"+str(options.mass))
+    #bias('combined.txt','combined.txt',options.toys,options.sig,"fitbase"+str(options.mass))
     #plotmass('card_ralpha.txt',options.mass)
+
+
+    #ftest('card_rhalphabet_r2p2.txt','card_rhalphabet_r3p2.txt',1000,'ftest_r2p2_v_r3p2')
+
+    ## nllBase=nllDiff("base1.root","base2.root",p1=11,p2=8)
+    ## nllToys=nllDiff("toys1.root","toys2.root",p1=11,p2=8)
+    ## lPass=0
+    ## for val in nllToys:
+    ##     print val,nllBase[0]
+    ##     if val < nllBase[0]:
+    ##         lPass+=1
+    ## print "ftest prob",float(lPass)/float(len(nllToys))
+    ## plotftest(nllToys,nllBase[0],float(lPass)/float(len(nllToys)),'ftest_r2p2_v_r3p2')
+
+    if options.method=='GoodnessOfFit':
+        iLabel= 'goodness_%s'%(options.datacard.split('/')[-1].replace('.txt',''))
+        goodness(options.datacard, options.toys, iLabel, options)
+
+    elif options.method=='MaxLikelihoodFit':
+        fit(options.datacard,options)
+
+    elif options.method=='FTest':
+        iLabel= 'ftest_%s_vs_%s'%(options.datacard.split('/')[-1].replace('.txt',''),options.datacardAlt.split('/')[-1].replace('.txt',''))
+        ftest(options.datacard, options.datacardAlt, options.toys, iLabel, options)
+        
