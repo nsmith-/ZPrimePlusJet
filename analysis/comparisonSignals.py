0d02aa15466e219fd1a4146e361adf05d987fa1b
Caterina <caterina.vernieri@cern.ch>
2017-01-22 11:23:33 -0600

    fix naming v12

--- analysis/comparisonSignals.py
+++ analysis/comparisonSignals.py
@@ -30,14 +30,14 @@ def main(options,args):
 	       'Phibb': ' Phi(125)(b#bar{b})'}
 
         
-    tfiles = {'ggHbb': [idir+'/GluGluHToBB_M125_13TeV_powheg_pythia8_1000pb_weighted.root'],#amcatnloFXFX_pythia8_1000pb_weighted.root'],
-       	      'ggHbbp': [idir+'/GluGluHToBB_M125_13TeV_powheg_pythia8_1000pb_weighted_now.root'],
-              'VBFHbb': [idir+'/VBFHToBB_M125_13TeV_amcatnlo_pythia8_1000pb_weighted.root'],
-	      'ZHbb': [idir+'/ZH_HToBB_ZToQQ_M125_13TeV_powheg_pythia8_1000pb_weighted.root'],
-	      'ZnnHbb': [idir+'/ggZH_HToBB_ZToNuNu_M125_13TeV_powheg_pythia8_1000pb_weighted.root',idir+'/ZH_HToBB_ZToNuNu_M125_13TeV_powheg_pythia8_ext_1000pb_weighted.root'],
-	      'WHbb' : [idir+'/WminusH_HToBB_WToQQ_M125_13TeV_powheg_pythia8_1000pb_weighted.root', idir+'/WplusH_HToBB_WToQQ_M125_13TeV_powheg_pythia8_1000pb_weighted.root'],
-	      'tthbb' : [idir+'/ttHTobb_M125_13TeV_powheg_pythia8_1000pb_weighted.root'],
-	      'Phibb':[idir+'/DMSpin0_ggPhibb1j_125_1000pb_weighted.root']
+    tfiles = {'ggHbb': [idir+'/GluGluHToBB_M125_13TeV_powheg_pythia8_0_ptw_1000pb_weighted.root'],#amcatnloFXFX_pythia8_1000pb_weighted.root'],
+       	      'ggHbbp': [idir+'/GluGluHToBB_M125_13TeV_powheg_pythia8_0_1000pb_weighted.root'],
+              'VBFHbb': [idir+'/VBFHToBB_M125_13TeV_amcatnlo_pythia8_0_1000pb_weighted.root'],
+	      'ZHbb': [idir+'/ZH_HToBB_ZToQQ_M125_13TeV_powheg_pythia8_0_1000pb_weighted.root'],
+	      'ZnnHbb': [idir+'/ggZH_HToBB_ZToNuNu_M125_13TeV_powheg_pythia8_0_1000pb_weighted.root'],#idir+'/ZH_HToBB_ZToNuNu_M125_13TeV_powheg_pythia8_ext_1000pb_weighted.root'],
+	      'WHbb' : [idir+'/WminusH_HToBB_WToQQ_M125_13TeV_powheg_pythia8_0_1000pb_weighted.root'],#idir+'/WplusH_HToBB_WToQQ_M125_13TeV_powheg_pythia8_0_1000pb_weighted.root'],
+	      'tthbb' : [idir+'/ttHTobb_M125_TuneCUETP8M2_ttHtranche3_13TeV_powheg_pythia8_0_1000pb_weighted.root'],
+	      'Phibb':[idir+'/Spin0_ggPhi12j_g1_125_Scalar_13TeV_madgraph_0_1000pb_weighted.root']
                }
 
     color = {'ggHbb': ROOT.kBlack,
@@ -50,7 +50,7 @@ def main(options,args):
 	     'Phibb':ROOT.kRed-2
                }
 
-    style = {'ggHbb': 2,
+    style = {'ggHbb': 1,
 	     'ggHbbp': 1,
              'VBFHbb': 2,
 	     'ZHbb': 1,
