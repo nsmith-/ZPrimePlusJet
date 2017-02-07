b3f71d2c7295b2e921815ba6555b429ed5a89517
Caterina <caterina.vernieri@cern.ch>
2017-02-07 17:21:56 -0600

    fix mass corrections

--- analysis/sampleContainer.py
+++ analysis/sampleContainer.py
@@ -34,10 +34,10 @@ class sampleContainer:
             self._lumi = 1
         self._fillCA15 = fillCA15
 
-	f_puppi= ROOT.TFile.Open("$ZPRIMEPLUSJET_BASE/analysis/ZqqJet/puppiCorr.root","read")
-  	self._puppisd_corrGEN      = f_puppi.Get("puppiJECcorr_gen")
-  	self._puppisd_corrRECO_cen = f_puppi.Get("puppiJECcorr_reco_0eta1v3")
-  	self._puppisd_corrRECO_for = f_puppi.Get("puppiJECcorr_reco_1v3eta2v5")
+	#f_puppi= ROOT.TFile.Open("$ZPRIMEPLUSJET_BASE/analysis/ZqqJet/puppiCorr.root","read")
+  	#self._puppisd_corrGEN      = f_puppi.Get("puppiJECcorr_gen")
+  	#self._puppisd_corrRECO_cen = f_puppi.Get("puppiJECcorr_reco_0eta1v3")
+  	#self._puppisd_corrRECO_for = f_puppi.Get("puppiJECcorr_reco_1v3eta2v5")
 	
 	#f_pu= ROOT.TFile.Open("$ZPRIMEPLUSJET_BASE/analysis/ggH/puWeights_Jan11.root","read")
         #self._puw      = f_pu.Get("puw")
@@ -335,6 +335,7 @@ class sampleContainer:
             jpt_8  = self.AK8Puppijet0_pt[0]
             jeta_8  = self.AK8Puppijet0_eta[0]
 	    jmsd_8 = self.AK8Puppijet0_msd[0]*self.PUPPIweight(jpt_8,jeta_8)
+	    print(jmsd_8_raw, jpt_8, self.PUPPIweight(jpt_8,jeta_8))
 
             jpt_8_sub1  = self.AK8Puppijet1_pt[0]
             jpt_8_sub2  = self.AK8Puppijet2_pt[0]
@@ -743,11 +744,36 @@ class sampleContainer:
         genCorr  = 1.
         recoCorr = 1.
         totalWeight = 1.
-        genCorr =  self._puppisd_corrGEN.Eval( puppipt )
+	#based on https://github.com/thaarres/PuppiSoftdropMassCorr Summer16 
+	corrGEN = ROOT.TF1("corrGEN","[0]+[1]*pow(x*[2],-[3])",200,3500)
+	corrGEN.SetParameter(0,1.00626)
+	corrGEN.SetParameter(1, -1.06161)
+	corrGEN.SetParameter(2,0.0799900)
+	corrGEN.SetParameter(3,1.20454)
+
+	corrRECO_cen = ROOT.TF1("corrRECO_cen","[0]+[1]*x+[2]*pow(x,2)+[3]*pow(x,3)+[4]*pow(x,4)+[5]*pow(x,5)",200,3500)
+	corrRECO_cen.SetParameter(0,1.09302)
+	corrRECO_cen.SetParameter(1,-0.000150068)
+	corrRECO_cen.SetParameter(2,3.44866e-07)
+	corrRECO_cen.SetParameter(3,-2.68100e-10)
+	corrRECO_cen.SetParameter(4,8.67440e-14)
+	corrRECO_cen.SetParameter(5,-1.00114e-17)
+
+        corrRECO_for = ROOT.TF1("corrRECO_for","[0]+[1]*x+[2]*pow(x,2)+[3]*pow(x,3)+[4]*pow(x,4)+[5]*pow(x,5)",200,3500)
+        corrRECO_for.SetParameter(0,1.27212)
+        corrRECO_for.SetParameter(1,-0.000571640)
+        corrRECO_for.SetParameter(2,8.37289e-07)
+        corrRECO_for.SetParameter(3,-5.20433e-10)
+        corrRECO_for.SetParameter(4,1.45375e-13)
+        corrRECO_for.SetParameter(5,-1.50389e-17)
+
+
+        genCorr =  corrGEN.Eval( puppipt )
   	if( abs(puppieta)  < 1.3 ):
-    		recoCorr = self._puppisd_corrRECO_cen.Eval( puppipt )
+    		recoCorr = corrRECO_cen.Eval( puppipt )
     	else: 
-		recoCorr = self._puppisd_corrRECO_for.Eval( puppipt );
+		recoCorr = corrRECO_for.Eval( puppipt );
+	print(genCorr,recoCorr)
 	totalWeight = genCorr*recoCorr
   	return totalWeight
 
