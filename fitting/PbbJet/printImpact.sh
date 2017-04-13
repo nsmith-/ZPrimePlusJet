afff0cb6abac25da4176d05fa04af79017c37a1e
Javier Duarte <jduarte@caltech.edu>
2017-04-13 18:29:10 -0500

    Update printImpact.sh

--- fitting/PbbJet/printImpact.sh
+++ fitting/PbbJet/printImpact.sh
@@ -4,3 +4,15 @@ combineTool.py -M Impacts -d card_rhalphabet_muonCR.root -m 125 --doFits --robus
 combineTool.py -M Impacts -d card_rhalphabet_muonCR.root -m 125 -o impacts.json
 plotImpacts.py -i impacts.json -o impacts
 
+# javier commands:
+text2workspace.py -m 125 card_rhalphabet_muonCR.txt
+combineTool.py -M Impacts -d card_rhalphabet_muonCR.root -m 125  -t -1 --toysFreq --expectSignal 1 --robustFit 1 --doInitialFit --rMin -5 --rMax 5 --exclude 'rgx{mcstat}'
+combineTool.py -M Impacts -d card_rhalphabet_muonCR.root -m 125  -t -1 --toysFreq --expectSignal 1 --robustFit 1 --doFits --rMin -5 --rMax 5 --exclude 'rgx{mcstat}'
+combineTool.py -M Impacts -d card_rhalphabet_muonCR.root -m 125  -t -1 --toysFreq --expectSignal 1 -o impacts_asimov.json --rMin -5 --rMax 5 --exclude 'rgx{mcstat}'
+plotImpacts.py -i impacts_asimov.json -o impacts_asimov
+
+text2workspace.py -m 125 card_rhalphabet_muonCR.txt
+combineTool.py -M Impacts -d card_rhalphabet_muonCR.root -m 125 --robustFit 1 --doInitialFit --rMin -5 --rMax 5 --exclude 'rgx{mcstat}'
+combineTool.py -M Impacts -d card_rhalphabet_muonCR.root -m 125 --robustFit 1 --doFits --rMin -5 --rMax 5 --exclude 'rgx{mcstat}'
+combineTool.py -M Impacts -d card_rhalphabet_muonCR.root -m 125 -o impacts_data.json --rMin -5 --rMax 5 --exclude 'rgx{mcstat}'
+plotImpacts.py -i impacts_data.json -o impacts_data
