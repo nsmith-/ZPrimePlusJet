63c15b526496b2fea5d925995eec88a7bf7527a6
Caterina <caterina.vernieri@cern.ch>
2017-04-28 12:57:29 -0500

    switch to Hpt corr

--- fitting/PbbJet/Hbb_create.py
+++ fitting/PbbJet/Hbb_create.py
@@ -27,7 +27,7 @@ def main(options, args):
     outfile = ROOT.TFile(options.odir + "/" + fileName, "recreate")
 
     tfiles = {
-        'hqq125': [idir + '/GluGluHToBB_M125_13TeV_powheg_pythia8_all_1000pb_weighted.root'],
+        'hqq125': [idir + '/GluGluHToBB_M125_13TeV_powheg_pythia8_all_1000pb_weighted_corrected.root'],#GluGluHToBB_M125_13TeV_powheg_pythia8_all_1000pb_weighted.root'],
         # idir+'/GluGluHToBB_M125_13TeV_powheg_pythia8_ext_1000pb_weighted.root'],
         # 'VBFHbb': [idir+'/VBFHToBB_M125_13TeV_amcatnlo_pythia8_1000pb_weighted.root'],
         'vbfhqq125': [idir + '/VBFHToBB_M_125_13TeV_powheg_pythia8_weightfix_all_1000pb_weighted.root'],
