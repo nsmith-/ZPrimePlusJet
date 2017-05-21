5147196b2a491825cb2410f1b86b2827098aad59
Javier Duarte <jduarte@caltech.edu>
2017-05-21 13:32:33 -0400

    fix style

--- analysis/ZqqJet/plotDDT.py
+++ analysis/ZqqJet/plotDDT.py
@@ -10,15 +10,18 @@ import time
 
 ##############################################################################
 def main(options,args):
-    file_in = ROOT.TFile.Open("h3_n2ddt_26eff_36binrho11pt_Spring16.root");
+    
+    
+    c_ptH = ROOT.TCanvas("c_ptH","c_ptH",800,600)
+    file_in = ROOT.TFile.Open("h3_n2ddt_26eff_36binrho11pt_Spring16.root")
     file_in.cd()
-    tt = file_in.Get('h2ddt')
-
-
-    c_ptH = ROOT.TCanvas("c_ptH","c_ptH",800,800);
-
-    c_ptH.cd();
-    ROOT.gStyle.SetPalette(ROOT.kBlackBody);
+    
+    tt = file_in.Get('h2ddt').Clone()
+    
+    tt.Draw('COLZ')
+    tt.SetContour(999)
+    ROOT.gPad.Update()
+    #c_ptH.cd()
     tt.GetXaxis().SetTitle('#rho = ln(m_{SD}^{2}/p_{T}^{2})')
     tt.GetYaxis().SetTitle('p_{T} (GeV)')
     tt.GetXaxis().SetTitleSize(0.043)
@@ -27,12 +30,11 @@ def main(options,args):
     tt.GetYaxis().SetTitleOffset(1.2)
     tt.GetXaxis().SetLabelSize(0.03)
     tt.GetYaxis().SetLabelSize(0.03)
-    tt.GetZaxis().SetLabelSize(0.025)
+    tt.GetZaxis().SetLabelSize(0.03)
     tt.GetZaxis().SetTitleSize(0.04)
     tt.GetZaxis().SetTitleOffset(1.2)
     tt.GetZaxis().SetTitle('N_{2}^{1} cut at 26% QCD eff')
-    ROOT.gStyle.SetNumberContours(999)
-    palette = ROOT.TPaletteAxis(-1.74753,450.4435,-1.501524,1000.444,tt);
+    palette = ROOT.TPaletteAxis(-1.45,450,-1.1,1000,tt)
     tt.Draw('COL')
     palette.Draw()
     tag1 = ROOT.TLatex(0.67, 0.92, "35.9 fb^{-1} (13 TeV)")
@@ -60,16 +62,17 @@ if __name__ == '__main__':
 
     (options, args) = parser.parse_args()
 
-    #import tdrstyle
-    #tdrstyle.setTDRStyle()
+    import tdrstyle
+    tdrstyle.setTDRStyle()
     ROOT.gStyle.SetPadTopMargin(0.10)
     ROOT.gStyle.SetPadLeftMargin(0.16)
-    ROOT.gStyle.SetPadRightMargin(0.10)
-    ROOT.gStyle.SetPalette(1)
+    ROOT.gStyle.SetPadRightMargin(0.17)
     ROOT.gStyle.SetPaintTextFormat("1.1f")
     ROOT.gStyle.SetOptStat(0000)
     ROOT.gStyle.SetOptFit(0000)
     ROOT.gROOT.SetBatch()
+    ROOT.gStyle.SetPalette(ROOT.kBlackBody)
+    ROOT.gStyle.SetNumberContours(999)
 
     main(options,args)
 ##----##----##----##----##----##----##
