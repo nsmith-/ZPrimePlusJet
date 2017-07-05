b0cc5249bb30d39ae3b9368c2b564869c0352d21
Anterpreet <anter.18@gmail.com>
2017-07-05 15:52:52 -0500

    adding muonCR in script

--- fitting/PbbJet/script_cards.sh
+++ fitting/PbbJet/script_cards.sh
@@ -1,2 +1,4 @@
-nohup python Hbb_create_Phibb.py --lumi 35.9 --skip-data > output_ak8.txt &
-nohup python Hbb_create_Phibb.py --lumi 35.9 --fillCA15 --skip-data > output_ca15.txt &
+#AK8
+nohup python Hbb_create_Phibb.py --lumi 35.9 --skip-data --muonCR > output_ak8.txt &
+#CA15
+nohup python Hbb_create_Phibb.py --lumi 35.9 --fillCA15 --skip-data --muonCR > output_ca15.txt &
