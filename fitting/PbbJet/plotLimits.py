d445af214ba3d9c7fd2d342a35fb6f101447c69e
Javier Duarte <jduarte1@fnal.gov>
2017-07-25 10:33:32 -0500

    update plotting style

--- fitting/PbbJet/plotLimits.py
+++ fitting/PbbJet/plotLimits.py
@@ -1,9 +1,9 @@
 from optparse import OptionParser
-import ROOT
-from ROOT import TFile, TTree, TCanvas, TGraph, TMultiGraph, TGraphErrors, TLegend
+import ROOT as rt
 import CMS_lumi, tdrstyle
 import subprocess # to execute shell command
-ROOT.gROOT.SetBatch(ROOT.kTRUE)
+rt.gROOT.SetBatch(True)
+import glob
 
 # CMS style
 CMS_lumi.cmsText = "CMS"
@@ -12,6 +12,40 @@ CMS_lumi.cmsTextSize = 0.65
 CMS_lumi.outOfFrame = True
 tdrstyle.setTDRStyle()
 
+
+# GET limits from root file
+def getLimits(file_name):
+    
+    tfile = rt.TFile.Open(file_name,'read')
+    try:
+        if tfile.InheritsFrom("TFile") is False:
+            print 'tfile.InheritsFrom("TFile") is False'
+            return []
+    except:
+        print 'EXCEPTION'
+        return []
+        
+    tree = tfile.Get("limit")
+    
+    try:
+        if tree.InheritsFrom("TTree") is False: 
+            print 'limit.InheritsFrom("TTree") is False'
+            tfile.cd()
+            tfile.Close()
+            return []
+    except:
+        print 'EXCEPTION'
+        tfile.cd()
+        tfile.Close()
+        return []
+
+    limits = []
+    for quantile in tree:
+        limits.append(tree.limit)
+
+    return limits
+
+
 def massIterable(massList):
     if len(massList.split(','))==1:
         massIterableList = [massList]
@@ -19,53 +53,64 @@ def massIterable(massList):
         massIterableList = list(eval(massList))
     return massIterableList
 
-
-# GET limits from root file
-def getLimits(file_name):
-
-    file = TFile(file_name)
-    tree = file.Get("limit")
-
-    limits = [ ]
-    for quantile in tree:
-        limits.append(tree.limit)
-        #print ">>>   %.2f" % limits[-1]
-
-    return limits[:6]
-
+xsections = {'50': 1.574e-02*0.8,
+            '100': 1.526e-02*0.8,
+            '125': 1.486e-02*0.8,
+            '200': 1.359e-02*0.8,
+            '300': 1.251e-02*0.8,
+            '350': 1.275e-02*0.8,
+            '400': 1.144e-02*0.8,
+            '500': 7.274e-03*0.8
+            }
 
 # PLOT upper limits
-def plotUpperLimits():
+def plotUpperLimits(options,args):
     # see CMS plot guidelines: https://ghm.web.cern.ch/ghm/plots/
-    masses = [50, 100, 125, 200, 300, 350, 400, 500]
-    xsection = [1.574e-02, 1.526e-02, 1.486e-02, 1.359e-02, 1.251e-02, 1.275e-02, 1.144e-02, 7.274e-03]
+    all_masses = [50, 100, 125, 200, 300, 350, 400, 500]
+    masses = []
+    jet_type = options.box
+    cut = options.cuts.split(',')[0]
+    file_names = {}
+    limits = {}
+    for mass in all_masses:
+        file_name = options.idir + "/%s/%s/DMSbb%s/higgsCombineDMSbb_%s_lumi-%.1f_%s.Asymptotic.mH120.root"%(jet_type,cut,str(mass),str(mass),options.lumi,jet_type)
+        print file_name
+        if glob.glob(file_name):
+            print "Opened File ", file_name
+            file_names[str(mass)] = file_name
+            limits[str(mass)] = getLimits(file_name)
+            print len( limits[str(mass)])
+            if len( limits[str(mass)])>0:
+                masses.append(mass)
+            
+            
     N = len(masses)
     print " No of mass points : ", N
-    yellow = TGraph(2*N)    # yellow band
-    green = TGraph(2*N)     # green band
-    median = TGraph(N)      # median line
-    obs = TGraph(N)       # observed
-    if options.xsec: theory_xsec = TGraph(N)       # theory cross section
-    jet_type = 'AK8'
-    if options.fillCA15:
-        jet_type = 'CA15'
-    cut = options.cuts.split(',')[0]   
+    yellow = rt.TGraph(2*N)    # yellow band
+    green = rt.TGraph(2*N)     # green band
+    median = rt.TGraph(N)      # median line
+    obs = rt.TGraph(N)       # observed
+    if options.xsec:
+        theory_xsec = rt.TGraph(N)       # theory cross section
 
     up2s = [ ]
-    for i in range(N):
-        file_name = options.ifile+ "/%s/%s/DMSbb%s/higgsCombineTest.Asymptotic.mH120.root"%(jet_type,cut,str(masses[i]))
-        print "Opened File ", file_name
-        limit = getLimits(file_name)
+    i = -1
+    for mass in masses:
+        i += 1
+        limit = limits[str(mass)]
         up2s.append(limit[4])
-        if options.xsec: fac = xsection[i]
-        else: fac = 1
-        yellow.SetPoint(    i,    masses[i], limit[4] * fac ) # + 2 sigma
-        green.SetPoint(     i,    masses[i], limit[3] * fac ) # + 1 sigma
-        median.SetPoint(    i,    masses[i], limit[2] * fac ) # median
-        green.SetPoint(  2*N-1-i, masses[i], limit[1] * fac ) # - 1 sigma
-        yellow.SetPoint( 2*N-1-i, masses[i], limit[0] * fac ) # - 2 sigma
-        obs.SetPoint(       i,    masses[i], limit[5] * fac) # observed
-        if options.xsec : theory_xsec.SetPoint(       i,    masses[i], xsection[i]) # theory x-section
+        if options.xsec:
+            fac = xsections[str(mass)]
+        else:
+            fac = 1
+        yellow.SetPoint(    i,    mass, limit[4] * fac ) # + 2 sigma
+        green.SetPoint(     i,    mass, limit[3] * fac ) # + 1 sigma
+        median.SetPoint(    i,    mass, limit[2] * fac ) # median
+        green.SetPoint(  2*N-1-i, mass, limit[1] * fac ) # - 1 sigma
+        yellow.SetPoint( 2*N-1-i, mass, limit[0] * fac ) # - 2 sigma
+        obs.SetPoint(       i,    mass, limit[5] * fac) # observed
+        if options.xsec:
+            theory_xsec.SetPoint(       i,    mass, xsections[str(mass)]) # theory x-section
 
     W = 800
     H  = 600
@@ -73,7 +118,7 @@ def plotUpperLimits():
     B = 0.12*H
     L = 0.12*W#*scaleleftmargin
     R = 0.04*W#*scalerightmargin
-    c = TCanvas("c","c",100,100,W,H)
+    c = rt.TCanvas("c","c",100,100,W,H)
     c.SetFillColor(0)
     c.SetBorderMode(0)
     c.SetFrameFillStyle(0)
@@ -96,7 +141,7 @@ def plotUpperLimits():
     frame.GetXaxis().SetNdivisions(508)
     frame.GetYaxis().CenterTitle(True)
     #frame.GetYaxis().SetTitle("95% upper limit on #sigma / #sigma_{SM}")
-#    frame.GetYaxis().SetTitle("95% upper limit on #sigma #times BR / (#sigma #times BR)_{SM}")
+    #frame.GetYaxis().SetTitle("95% upper limit on #sigma #times BR / (#sigma #times BR)_{SM}")
     #frame.GetXaxis().SetTitle("background systematic uncertainty [%]")
     if options.xsec: 
         frame.SetMinimum(0.001)
@@ -104,15 +149,34 @@ def plotUpperLimits():
     else:
         frame.SetMinimum(0)
         frame.SetMaximum(max(up2s)*1.05)
-    frame.GetXaxis().SetLimits(min(masses),max(masses))
-
-    yellow.SetFillColor(ROOT.kOrange)
-    yellow.SetLineColor(ROOT.kOrange)
-    yellow.SetFillStyle(1001)
-    yellow.Draw('F')
     
-    green.SetFillColor(ROOT.kGreen+1)
-    green.SetLineColor(ROOT.kGreen+1)
+    h_limit = rt.TMultiGraph()
+    h_limit.Add(yellow)
+    h_limit.Add(green)
+    h_limit.Add(median)
+    #h_limit.Add(obs)
+    #h_limit.Add(theory_xsec)
+
+    h_limit.Draw('a3')
+    h_limit.GetXaxis().SetLimits(options.massMin,options.massMax)
+    h_limit.SetMinimum(options.xsecMin)
+    h_limit.SetMaximum(options.xsecMax)
+    h_limit.GetXaxis().SetTitle('Resonance mass [GeV]')
+    h_limit.GetYaxis().SetTitle("#sigma [pb]")
+    h_limit.GetYaxis().SetTitleOffset(0.9)
+    #h_limit.Draw('F')
+    
+    yellow.SetFillColor(rt.kOrange)
+    yellow.SetLineColor(rt.kBlack)
+    yellow.SetFillStyle(1001)
+    yellow.SetLineWidth(2)
+    yellow.SetLineStyle(2)
+    yellow.Draw('Fsame')
+    
+    green.SetFillColor(rt.kGreen+1)
+    green.SetLineColor(rt.kBlack)
+    green.SetLineWidth(2)
+    green.SetLineStyle(2)
     green.SetFillStyle(1001)
     green.Draw('Fsame')
 
@@ -127,53 +191,63 @@ def plotUpperLimits():
 
     if options.xsec:
         theory_xsec.SetMarkerStyle(20)
+        theory_xsec.SetLineColor(rt.kBlue+2)
         theory_xsec.SetLineWidth(2)
         theory_xsec.Draw('Lsame')
 
-    CMS_lumi.CMS_lumi(c,13,11)
-    ROOT.gPad.SetTicks(1,1)
-    frame.Draw('sameaxis')
+    CMS_lumi.lumi_13TeV = "%.1f fb^{-1}"%options.lumi
+    CMS_lumi.CMS_lumi(c,4,11)
 
-    x1 = 0.22
+    #rt.gPad.SetTicks(1,1)
+    #frame.Draw('sameaxis')
+
+    x1 = 0.65
     x2 = x1 + 0.24
-    if options.xsec: y1 = 0.68
-    else: y1 = 0.58
-    y2 = y1 + 0.16
-    legend = TLegend(x1,y1,x2,y2)
+    if options.xsec: 
+        y1 = 0.72
+    else: 
+        y1 = 0.62
+    y2 = y1 + 0.18
+    legend = rt.TLegend(x1,y1,x2,y2)
     legend.SetFillStyle(0)
     legend.SetBorderSize(0)
     legend.SetTextSize(0.041)
     legend.SetTextFont(42)
     legend.AddEntry(obs, "Observed",'lp')
-    legend.AddEntry(median, "Asymptotic CL_{s} expected",'L')
-    legend.AddEntry(green, "#pm 1 std. deviation",'f')
-    #legend.AddEntry(green, "Asymptotic CL_{s} #pm 1 std. deviation",'f')
-    legend.AddEntry(yellow,"#pm 2 std. deviation",'f')
-    if options.xsec: legend.AddEntry(theory_xsec,"Theory x-sec",'l')
-    #legend.AddEntry(yellow, "Asymptotic CL_{s} #pm 2 std. deviation",'f')
+    #legend.AddEntry(median, "Asymptotic CL_{s} expected",'L')
+    legend.AddEntry(green, "Expeted #pm 1 s.d.",'lf')
+    legend.AddEntry(yellow,"Expected #pm 2 s.d.",'lf')
+    if options.xsec:
+        legend.AddEntry(theory_xsec,"#Phi(b#bar{b})",'l')
     legend.Draw()
 
     print " "
-    if options.xsec: c.SetLogy()
-    if options.xsec: c.SaveAs("Limit_" + jet_type + "_" + cut+ "_xsec.png") 
-    else: c.SaveAs("Limit_" + jet_type + "_" + cut+ ".png")
-    #c.SaveAs("Limit_" + jet_type + "_" + cut+ ".C")    
+    if options.xsec: 
+        c.SetLogy()
+        c.SaveAs("Limit_" + jet_type + "_" + cut+ "_xsec.pdf") 
+        c.SaveAs("Limit_" + jet_type + "_" + cut+ "_xsec.C") 
+    else: 
+        c.SaveAs("Limit_" + jet_type + "_" + cut+ ".pdf")
+        c.SaveAs("Limit_" + jet_type + "_" + cut+ ".C")
     c.Close()
 
 # MAIN
 def main(options,args):
-
-    plotUpperLimits()
-
-
+    plotUpperLimits(options,args)
 
 if __name__ == '__main__':
     parser = OptionParser()
+    parser.add_option('--model',dest="model", default="DMSbb",type="string", help="signal model name")
+    parser.add_option('--mass',dest="mass", default='750',type="string", help="mass of resonance")
+    parser.add_option('-b','--box',dest="box", default="AK8",type="string", help="box name")
     parser.add_option("--lumi", dest="lumi", default=35.9, type="float", help="luminosity", metavar="lumi")
     parser.add_option('-c', '--cuts', dest='cuts', default='p9', type='string', help='double b-tag cut value')
     parser.add_option('-x','--xsec', dest='xsec', action='store_true',default=False, help='cross_section',metavar='xsec')
-    parser.add_option('--fillCA15', action='store_true', dest='fillCA15', default =False,help='for CA15', metavar='fillCA15')
-    parser.add_option('-i', '--ifile', dest='ifile', default='hist_1DZbb.root', help='file with histogram inputs',metavar='ifile')
+    parser.add_option('-i', '--idir', dest='idir', default='./', help='input directory',metavar='idir')
+    parser.add_option('--massMin',dest="massMin", default=50.,type="float", help="minimum mass")
+    parser.add_option('--massMax',dest="massMax", default=500.,type="float", help="maximum mass")
+    parser.add_option('--xsecMin',dest="xsecMin", default=1e-3,type="float", help="minimum xsec")
+    parser.add_option('--xsecMax',dest="xsecMax", default=1e4,type="float", help="maximum xsec")
 
     (options,args) = parser.parse_args()   
     main(options,args) 
