c64d47c5b9055003e006381917fbad7c7a807398
Martin <ka_hei_martin_kwok@brown.edu>
2018-10-24 14:42:18 -0500

    bug fix for writeFilejson

--- analysis/writeFilejson.py
+++ analysis/writeFilejson.py
@@ -3,27 +3,32 @@ import controlPlotsGGH
 import Hbb_create
 from optparse import OptionParser
 
+def expand(fpaths):
+    expandedPaths = []
+    for p in fpaths:
+        if "*" in p:
+            if "root://" in p:
+                redirector = "root://cmseos.fnal.gov/"
+                eosp = p.replace("root://cmseos.fnal.gov/","")  #glob does not work with redirector
+                globpaths = glob.glob(eosp)
+                for i,glob_p in enumerate(globpaths):
+                    globpaths[i] = redirector+glob_p
+                expandedPaths.extend(globpaths)
+        else:
+            expandedPaths.append(p)
+    return expandedPaths 
+
 def expandPath(fdict):
     rdict = {}
     for sample,subSample in fdict.iteritems():
         if type(subSample)==type([]):       # already filenames
-            rdict[sample] = subSample
+            rdict[sample] = expand(subSample)
+            if len(rdict[sample])==0:            print "ERROR: %s has no files"%(sample)
         elif type(subSample)==type({}):     # subSamples has list of files each, expand
             d={} 
             for subSname,subpaths in subSample.iteritems():
-                expandedPath = subpaths
-                for p in subpaths:
-                    if "*" in p:
-                        if "root://" in p:
-                            redirector = "root://cmseos.fnal.gov/"
-                            eosp = p.replace("root://cmseos.fnal.gov/","")  #glob does not work with redirector
-                            globpaths = glob.glob(eosp)
-                            for i,glob_p in enumerate(globpaths):
-                                globpaths[i] = redirector+glob_p
-                            expandedPath.extend(globpaths)
-                            expandedPath.remove(p)
-                if len(expandedPath)==0:
-                        print "ERROR: %s has no files"%(subSname)
+                expandedPath = expand(subpaths) 
+                if len(expandedPath)==0:            print "ERROR: %s has no files"%(subSname)
                 d[subSname] = expandedPath 
             rdict[sample] =  d
     return rdict
@@ -34,10 +39,11 @@ def main(options,args):
         print "Writing to ", os.path.expandvars("$ZPRIMEPLUSJET_BASE/analysis/ggH/samplefiles.json")
     finaljson = {}
     finaljson['controlPlotsGGH_2017'] = expandPath(controlPlotsGGH.get2017files()) 
+    #finaljson['Hbb_create_2017']      = expandPath(Hbb_create.get2017files(False)) 
     if not options.printOnly:
         outf.write((json.dumps(finaljson,indent=4)))
     else:
-        print (json.dumps(finaljson,indent=4))
+        print (json.dumps(finaljson,indent=4,sort_keys=True))
     for key,tfiles in finaljson.iteritems():
         print "list of samples used by %s =  "%key, sorted(tfiles.keys())
 
