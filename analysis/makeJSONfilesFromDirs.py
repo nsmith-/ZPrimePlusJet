81c00d525f1cfc815453f3978e1fcdb142997abf
Andrzej <novak5andrzej@gmail.com>
2019-01-05 06:41:10 -0600

    Hcc

--- analysis/makeJSONfilesFromDirs.py
+++ analysis/makeJSONfilesFromDirs.py
@@ -4,12 +4,13 @@ import os
 
 sampledirs = open(os.path.expandvars("$ZPRIMEPLUSJET_BASE/analysis/ggH/sampledirs.json"),"r")
 fdirs  = json.load(sampledirs)['Hbb_create_2017']
+tfiles = {}
 for key in fdirs.keys():
 	tfiles[key] = {}
 	for subd in fdirs[key]:
 		subdfiles = [ "root://cmseos.fnal.gov/" + subd + f for f in os.listdir(subd)]
 		tfiles[key][ subd.strip('/').split('/')[-1] ] = subdfiles
 
-with open('ggH/samplefiles.json', 'w') as fp:
+with open('ggH/newsamplefiles.json', 'w') as fp:
 	json.dump(tfiles, fp, indent=4, separators=(',', ': '))                                                                                                                                     
 
