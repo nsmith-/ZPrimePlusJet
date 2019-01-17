d5e8f52c7c3394a630c498285dd8b6b58c3ff91e
Andrzej <novak5andrzej@gmail.com>
2019-01-17 08:44:00 -0600

    Hcc

--- fitting/PbbJet/Hxx_create.py
+++ fitting/PbbJet/Hxx_create.py
@@ -172,7 +172,7 @@ def main(options, args):
             }
         if options.is2017:
             #dataSample = normSampleContainer('data_obs', tfiles['muon'], sfData, DBTMIN, lumi, True, False, '((triggerBits&4)&&passJson)', True,
-            #        iSplit = options.iSplit, maxSplit = options.maxSplit, treeName=def_treeName, doublebCut=dbtagcut).addPlots(plots)
+            #  #      iSplit = options.iSplit, maxSplit = options.maxSplit, treeName=def_treeName, doublebCut=dbtagcut).addPlots(plots)
             print "using 2017 data"
             if muonCR:
                 dataSample = normSampleContainer('data_obs', tfiles['muon'], sfData, DBTMIN, lumi, True, False, '((triggerBits&1)&&passJson)', True,
